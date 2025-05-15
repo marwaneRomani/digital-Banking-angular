dd7100410e29af36df8de83d4879194985a8ad83
marwane <marwanromani2018@gmail.com>
2025-05-15 14:13:41 +0100

    initial project setup

---
+++ src/app/app.config.ts
@@ -0,0 +1,28 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
+import { routes } from './app.routes';
+
+// Simple auth interceptor function
+function authInterceptor(req: any, next: any) {
+  const token = localStorage.getItem('auth_token');
+
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+
+  return next(req);
+}
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
+};
